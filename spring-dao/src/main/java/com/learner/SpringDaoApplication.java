665df1a869f8b52de5e94e6a7b45cdae7330ccd5
pooja <poojamaurya596@gmail.com>
2021-12-05 18:01:41 +0530

    7. Implementing JdbcTemplate RowMapper

--- spring-dao/src/main/java/com/learner/SpringDaoApplication.java
+++ spring-dao/src/main/java/com/learner/SpringDaoApplication.java
@@ -10,39 +10,64 @@ import com.learner.jdbc.TopicJdbcDaoImpl;
 import com.learner.jpa.TopicJpaDaoImpl;
 
 @SpringBootApplication
-public class SpringDaoApplication implements CommandLineRunner{
+public class SpringDaoApplication implements CommandLineRunner {
 
 	@Autowired
 	TopicJdbcDaoImpl topicJdbcDaoImpl;
-	
+
 	@Autowired
 	TopicJpaDaoImpl topicJpaDaoImpl;
-	
+
 	@Autowired
 	TopicCoreJdbcDaoImpl topicCoreJdbcDaoImpl;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDaoApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {		
+	public void run(String... args) throws Exception {
 		System.out.println("TopicJdbcDaoImpl.insert() start!!");
 		topicJdbcDaoImpl.insert();
 		System.out.println("TopicJdbcDaoImpl.insert() end!!");
 		
+		System.out.println();
+
 		System.out.println("TopicJpaDaoImpl.insert() start!!");
 		topicJpaDaoImpl.insert();
-		System.out.println("TopicJpaDaoImpl.insert() end!!");	
+		System.out.println("TopicJpaDaoImpl.insert() end!!");
+
+		System.out.println();
 		
 		System.out.println("TopicCoreJdbcDaoImpl.insert() start!!");
 		topicCoreJdbcDaoImpl.insert();
-		System.out.println("TopicCoreJdbcDaoImpl.insert() end!!");	
+		System.out.println("TopicCoreJdbcDaoImpl.insert() end!!");
 		
+		System.out.println();
+
 		System.out.println("TopicCoreJdbcDaoImpl.insertByUsingDataSource() start!!");
 		topicCoreJdbcDaoImpl.insertByUsingDataSource();
-		System.out.println("TopicCoreJdbcDaoImpl.insertByUsingDataSource() end!!");	
+		System.out.println("TopicCoreJdbcDaoImpl.insertByUsingDataSource() end!!");
 		
+		System.out.println();
+
+		System.out.println("topicJdbcDaoImpl.getCount() start!!");
+		topicJdbcDaoImpl.getCount();
+		System.out.println("topicJdbcDaoImpl.getCount() end!!");
+		
+		System.out.println();
+
+		System.out.println("topicJdbcDaoImpl.getTopic() start!!");
+		topicJdbcDaoImpl.getTopic(1);
+		System.out.println("topicJdbcDaoImpl.getTopic() end!!");
+		
+		System.out.println();
+
+		System.out.println("topicJdbcDaoImpl.getAllTopic() start!!");
+		System.out.println(topicJdbcDaoImpl.getAllTopic().size());
+		System.out.println("topicJdbcDaoImpl.getAllTopic() end!!");
+		
+		System.out.println();
 	}
 
 }
--- spring-dao/src/main/java/com/learner/jdbc/TopicJdbcDaoImpl.java
+++ spring-dao/src/main/java/com/learner/jdbc/TopicJdbcDaoImpl.java
@@ -1,8 +1,15 @@
 package com.learner.jdbc;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.learner.entity.Topic;
 
 @Repository
 public class TopicJdbcDaoImpl {
@@ -16,6 +23,54 @@ public class TopicJdbcDaoImpl {
 		jdbcTemplate.update(sql);
 
 		System.out.println("Topic inserted successfully using JdbcTemplate!!!!");
+
+	}
+
+	public int getCount() {
+
+		String sql = "select count(*) from topic";
+
+		Integer topicCount = jdbcTemplate.queryForObject(sql, Integer.class);
+
+		System.out.println("Topic Count :" + topicCount);
+		return topicCount;
+
+	}
+
+	public Topic getTopic(int topicId) {
+
+		String sql = "select * from topic where id= ?";
+
+		List<Topic> topicList = jdbcTemplate.query(sql, new TopicRowMapper(), new Object[] { topicId });
+
+		Topic topic = topicList.size() > 0 ? topicList.get(0) : null;
+
+		System.out.println("Topic :" + topic);
+		return topic;
+
+	}
+
+	public List<Topic> getAllTopic() {
+
+		String sql = "select * from topic ";
+
+		List<Topic> topicList = jdbcTemplate.query(sql, new TopicRowMapper());
+
+		return topicList;
+
+	}
+
+	private static final class TopicRowMapper implements RowMapper<Topic> {
+
+		@Override
+		public Topic mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Topic topic = new Topic();
+			topic.setId(rs.getInt("ID"));
+			topic.setName(rs.getString("Name"));
+			topic.setDescription(rs.getString("Description"));
+
+			return topic;
+		}
 
 	}
 
